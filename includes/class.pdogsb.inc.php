9a0fb5f15ecb107d6a1d1ed4aa9e2fda3b8a1c94
Fernandez Tony <it-fernandeztony@gmail.com>
2021-02-20 11:52:33 +0100

    Retrait du dossier nbproject

--- includes/class.pdogsb.inc.php
+++ includes/class.pdogsb.inc.php
@@ -40,8 +40,8 @@ class PdoGsb
 {
     private static $serveur = 'mysql:host=localhost';
     private static $bdd = 'dbname=gsb_frais';
-    private static $user = 'userGsb';
-    private static $mdp = 'secret';
+    private static $user = 'root';
+    private static $mdp = '';
     private static $monPdo;
     private static $monPdoGsb = null;
 
